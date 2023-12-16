59ca9b95d5f788b9c23c6d41f0dcd9e0717c75c9
inmin617 <144991175+inmin617@users.noreply.github.com>
2023-12-16 19:14:39 +0900

    12/16

--- smm_object.h
+++ smm_object.h
@@ -18,6 +18,11 @@
 
 #define SMMNODE_TYPE_MAX                7
 
+typedef enum smmObjType {
+    smmObjType_board = 0,
+    smmObjType_card,
+    smmObjType_grade
+} smmObjType_e;
 
 /* node type :
     lecture,
@@ -45,11 +50,14 @@
 
 
 //object generation
-void smmObj_genNode(char* name, int type, int credit, int energy);
+void smmObj_genObject(char* name, smmObjType_e objType, int type, int credit, int energy, smmObjGrade_e grade);
+
 
 //member retrieving
 char* smmObj_getNodeName(int node_nr);
 int smmObj_getNodeType(int node_nr);
+int smmObj_getNodeCredit(int node_nr);
+int smmObj_getNodeEnergy(int node_nr);
 
 //element to string
 char* smmObj_getTypeName(int type);
